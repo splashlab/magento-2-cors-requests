d81e1b99ebc08e9775c8d3854fa6ee4bdffa2a17
Evan Johnson <thaddeusmt@gmail.com>
2018-09-11 11:23:37 -0600

    Fixing issue #6 where the Phrase object was used withont importing, instead of just using the __() global translate function.

--- Plugin/CorsRequestOptionsPlugin.php
+++ Plugin/CorsRequestOptionsPlugin.php
@@ -7,6 +7,7 @@
 namespace SplashLab\CorsRequests\Plugin;
 
 use Magento\Framework\Webapi\Request;
+use Magento\Framework\Exception\InputException;
 
 /**
  * Class CorsRequestOptionsPlugin
@@ -22,13 +23,13 @@ class CorsRequestOptionsPlugin
      *
      * @param Request $subject
      * @return void
-     * @throws \Magento\Framework\Exception\InputException
+     * @throws InputException
      */
     public function aroundGetHttpMethod(
         Request $subject
     ) {
         if (!$subject->isGet() && !$subject->isPost() && !$subject->isPut() && !$subject->isDelete() && !$subject->isOptions()) {
-            throw new \Magento\Framework\Exception\InputException(new Phrase('Request method is invalid.'));
+            throw new InputException(__('Request method is invalid.'));
         }
         return $subject->getMethod();
     }
